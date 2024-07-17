c554cfd394959a6024375ba46d985eab4949902a
Rishal Tauro <151727388+RishalTauro@users.noreply.github.com>
2024-07-17 09:49:10 +0530

    Add files via upload

---
+++ Criminal-Identification-System-master/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+@app.route('/')
+def home():
+    run_detection()  # Call the function to run home.py
+
+    return render_template('index.html')
+
+# @app.route('/detect', methods=['GET', 'POST'])
+# def detect():
+#     if request.method == 'POST':
+#         run_detection()  # Call the function to run home.py
+#         flash('Detection completed')
+#         return redirect(url_for('home'))
+#     else:
+#         return render_template('detect.html')
+
+def run_detection():
+    # Execute the logic from home.py here
+    os.system('python home.py')  # Replace with the appropriate command or code
+
+if __name__ == '__main__':
+    app.run(debug=True)
